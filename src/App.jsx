bd95742b1e4d11fdd9f846acfd8f99dffc7e9f4c
ania221B <100519699+ania221B@users.noreply.github.com>
2024-08-28 12:41:14 +0200

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import ArticleForm from './components/ArticleForm'
+import ArticleList from './components/ArticleList'
 import Card from './components/Card'
+import { useGlobalContext } from './context'
 
 function App () {
+  const { defaultArticle } = useGlobalContext()
   return (
     <main>
       <section className='section'>
-        <div className='container'>
+        <div className='container flow'>
           <h1>Card Generator</h1>
-          <Card></Card>
+          <ArticleForm></ArticleForm>
+          <Card article={defaultArticle}></Card>
+        </div>
+      </section>
+      <section className='section'>
+        <div className='container' data-container='large'>
+          <ArticleList></ArticleList>
         </div>
       </section>
     </main>
