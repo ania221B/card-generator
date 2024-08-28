0ab691569d27c594387339f31463a6285df0c57e
ania221B <100519699+ania221B@users.noreply.github.com>
2024-08-28 12:35:31 +0200

    card component made dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
-function Card () {
+function Card ({ article }) {
+  const { category, title, body, author, readTime, date, dateTime } = article
   return (
-    <article className='card flow'>
+    <article className='coffee-time card flow'>
       <div className='card__img'>
         <img
           src='./images/lidiya-pavlikova-y_gKWLrw3N4-unsplash.jpg'
@@ -10,21 +11,17 @@ function Card () {
       <div className='card__content'>
         <header>
           <div className='card__content__meta'>
-            <p className='category badge'>Food & Drink</p>
+            <p className='category badge'>{category}</p>
             <p className='read-time'>
-              <span>4 min</span> read
+              <span>{readTime}</span> min read
             </p>
             <p className='publish-date'>
-              <time datetime=''>August 19, 2024</time>
+              <time dateTime={dateTime}>{date}</time>
             </p>
           </div>
-          <h2 className='ff-secondary fs-800'>The Art and Science of Tea</h2>
+          <h2 className='ff-secondary fs-800'>{title}</h2>
         </header>
-        <p>
-          Tea, a timeless beverage, offers a world of flavours, health benefits,
-          and rituals. Discover how tea transforms from leaf to cup in this
-          insightful journey.
-        </p>
+        <p>{body}</p>
         <footer>
           <div className='card__content__author'>
             <picture>
@@ -33,7 +30,7 @@ function Card () {
                 alt='Lucy Whitmore'
               />
             </picture>
-            <p>Lucy Whitmore</p>
+            <p>{author}</p>
           </div>
         </footer>
       </div>
@@ -44,7 +41,18 @@ function Card () {
         className='button'
         button-type='outline'
       >
-        Read full article
+        <span>Read full article</span>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='32'
+          height='32'
+          viewBox='0 0 24 24'
+        >
+          <path
+            fill='currentColor'
+            d='M13.527 15.983q-.273.161-.49-.044t-.076-.497l1.637-2.942H3q-.213 0-.357-.143T2.5 12t.143-.357T3 11.5h11.598l-1.636-2.942q-.143-.293.075-.497t.49-.044l5.187 3.306q.378.243.378.677t-.378.677z'
+          />
+        </svg>
       </a>
     </article>
   )
