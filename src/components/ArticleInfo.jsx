402a011c82d9a17158aeee40dfaa889c32e0385e
ania221B <100519699+ania221B@users.noreply.github.com>
2024-09-03 20:11:26 +0200

    basic info input step added for article form

---
+++ src/components/ArticleInfo.jsx
@@ -0,0 +1,44 @@
+import { useGlobalContext } from '../context'
+import { categoryList } from '../data/data'
+import CategoryOption from './CategoryOption'
+import { nanoid } from 'nanoid'
+
+function ArticleInfo () {
+  const { step, article, handleChange } = useGlobalContext()
+  const { category, author } = article
+  return (
+    <section>
+      <h2>Step {step} of 4: Article Info</h2>
+      <div className='article-form__control-wrapper'>
+        <label htmlFor='article-category'>Category:</label>
+
+        <select
+          name='category'
+          id='article-category'
+          value={category}
+          onChange={e => handleChange(e)}
+        >
+          <option value=''>--Pick one option--</option>
+
+          {categoryList.map(item => {
+            return (
+              <CategoryOption optionName={item} key={nanoid()}></CategoryOption>
+            )
+          })}
+        </select>
+      </div>
+
+      <div className='article-form__control-wrapper'>
+        <label htmlFor='article-author'>Author:</label>
+        <input
+          id='article-author'
+          type='text'
+          name='author'
+          value={author}
+          onChange={e => handleChange(e)}
+        />
+      </div>
+    </section>
+  )
+}
+export default ArticleInfo
