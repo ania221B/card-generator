c9a107bab2e8711bd3eac1b12748e207c4c36ed2
ania221B <100519699+ania221B@users.noreply.github.com>
2024-09-03 20:10:54 +0200

    content input step for article form added

---
+++ src/components/ArticleContent.jsx
@@ -0,0 +1,33 @@
+import { useGlobalContext } from '../context'
+
+function ArticleContent () {
+  const { step, article, handleChange } = useGlobalContext()
+  const { title, body } = article
+  return (
+    <section>
+      <h2>Step {step} of 4: Article Content</h2>
+      <div className='article-form__control-wrapper'>
+        <label htmlFor='article-title'>Title:</label>
+        <input
+          id='article-title'
+          type='text'
+          name='title'
+          value={title}
+          onChange={e => handleChange(e)}
+        />
+      </div>
+      <div className='article-form__control-wrapper'>
+        <label htmlFor='article-body'>Text:</label>
+        <textarea
+          id='article-body'
+          type='text'
+          name='body'
+          value={body}
+          onChange={e => handleChange(e)}
+          rows={1}
+        ></textarea>
+      </div>
+    </section>
+  )
+}
+export default ArticleContent
