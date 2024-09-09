4231e9ab9029e5bbe66774bb7cb0335ba54d684d
ania221B <100519699+ania221B@users.noreply.github.com>
2024-09-09 09:33:50 +0200

    ArticleImage component added

---
+++ src/components/ArticleImage.jsx
@@ -0,0 +1,28 @@
+import { useGlobalContext } from '../context'
+import { imageList } from '../data/data'
+import RadioItem from './RadioItem'
+import { nanoid } from 'nanoid'
+
+function ArticleImage () {
+  const { step, article } = useGlobalContext()
+  return (
+    <section>
+      <h2>Step {step} of 5: Image</h2>
+      <fieldset>
+        <legend>Choose an image</legend>
+
+        {imageList.map(item => {
+          return (
+            <RadioItem
+              item={article.image}
+              itemName={item}
+              itemType='image'
+              key={nanoid()}
+            ></RadioItem>
+          )
+        })}
+      </fieldset>
+    </section>
+  )
+}
+export default ArticleImage
