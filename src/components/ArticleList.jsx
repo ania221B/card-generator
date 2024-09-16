545f706e434fe4464014634ec76294b72853a885
ania221B <100519699+ania221B@users.noreply.github.com>
2024-09-16 15:32:21 +0200

    added content that is displayed when there are no generated cards

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,8 +4,35 @@ import Card from './Card'
 
 function ArticleList () {
   const { articleList } = useGlobalContext()
+
+  if (articleList.length === 0) {
+    return (
+      <header
+        className='container'
+        data-container='small'
+        style={{
+          minHeight: '36rem',
+          display: 'grid',
+          placeItems: 'center',
+          maxWidth: '55ch',
+          marginBlock: 'auto'
+        }}
+      >
+        <h2>
+          You have no cards with article previews yet. Go to the previous page
+          to generate some ☺️
+        </h2>
+      </header>
+    )
+  }
   return (
-    <ul className='grid-auto-fit' role='list'>
+    <ul
+      className='grid-auto-fit'
+      role='list'
+      style={{
+        minHeight: '36rem'
+      }}
+    >
       {articleList.map(articleItem => {
         return (
           <li key={nanoid()}>
