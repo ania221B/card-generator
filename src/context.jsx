00ce7405de59d327566fd3491721aeff5a347241
ania221B <100519699+ania221B@users.noreply.github.com>
2024-09-17 21:20:51 +0200

    issue with datetime fixed

--- src/context.jsx
+++ src/context.jsx
@@ -263,7 +263,7 @@ function AppContext ({ children }) {
    */
   function getDateTimeString (date) {
     const year = date.getFullYear()
-    const month = date.getMonth().toString().padStart(2, '0')
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
     const day = date.getDate()
     return `${year}-${month}-${day}`
   }
